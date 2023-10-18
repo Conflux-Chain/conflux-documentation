d909f46ac0f8c3ea1afb7ade423e668515156dbe
darwintree <wenda.zhang@confluxnetwork.org>
2023-10-18 15:51:17 +0800

    l10n: spanish support

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,7 +24,7 @@ const config = {
   // to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
-    locales: ['en','zh-CN'],
+    locales: ['en','zh-CN','es'],
     localeConfigs: {
       "zh-CN": {
         path: "zh"
