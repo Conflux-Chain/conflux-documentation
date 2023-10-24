bf860ea42f2ab3fc19fb7f5ca00d44b51d38c04a
0xn1c0 <nico.conflux@gmail.com>
2023-10-24 10:50:39 -0300

    Added search feature

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -196,6 +196,36 @@ const config = {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
       },
+      algolia: {
+        // The application ID provided by Algolia
+        appId: 'EA5TTCRZGT',
+        
+        // Public API key: it is safe to commit it
+        apiKey: '459281c476ae57d1d2a275ea535cdf1c',
+        
+        indexName: 'confluxnetwork',
+        /*
+        // Optional: see doc section below
+        contextualSearch: true,
+        
+        // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+        externalUrlRegex: 'external\\.com|domain\\.com',
+        
+        // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+        replaceSearchResultPathname: {
+        from: '/docs/', // or as RegExp: /\/docs\//
+        to: '/docs/',
+        },
+        */
+        // Optional: Algolia search parameters
+        searchParameters: {},
+        
+        // Optional: path for search page that enabled by default (`false` to disable it)
+        searchPagePath: 'search',
+        
+        //... other Algolia params
+        },
+        
     }),
 };
 
