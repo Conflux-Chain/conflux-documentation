db88eb927d3aa7e72923a5422b44c881ff71724c
darwintree <17946284+darwintree@users.noreply.github.com>
2023-11-28 17:44:09 +0800

    Create checkGitCommitBash.sh

---
+++ scripts/checkGitCommitBash.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+current_branch=$(git rev-parse --abbrev-ref HEAD)
+
+if [ "$current_branch" != "l10n_main" ]; then
+    echo "current branch is not l10n_main (build needed)"
+    exit 1
+fi
+
+# Get latest commit
+files=$(git diff-tree --no-commit-id --name-only -r HEAD)
+
+# Check if only i18n files are modified
+for file in $files
+do
+    if [[ $file != i18n/* ]]
+    then
+        # build needed
+        exit 1
+    fi
+done
+
+# don't need build
+exit 0
