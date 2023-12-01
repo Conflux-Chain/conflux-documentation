3876f2c01a9edea745ef3817098b6770525acec6
PanaW <pana.wang@outlook.com>
2023-12-01 11:12:08 +0800

    update overall overview page, add more links

--- src/modules/docCatalog/index.js
+++ src/modules/docCatalog/index.js
@@ -18,8 +18,11 @@ const DocCatalogFackData = [
   {
     catalogue: 'Core Space',
     items: [
-      { url: '/docs/category/core-space-basics', text: 'Core Space Basic', preview: 'Learn about the Conflux Core Space' },
-      { url: '/docs/category/tutorials', text: 'Tutorials', preview: 'Tutorials for build Conflux Core Space' },
+      { url: '/docs/core/getting-started/', text: 'Getting Started', preview: 'Getting started using Fluent Wallet' },
+      { url: '/docs/category/core-space-basics', text: 'Core Space Basics', preview: 'Learn about the Conflux Core Space' },
+      { url: '/docs/core/core-space-basics/sponsor-mechanism', text: 'Sponsorship Mechanism', preview: 'Intro of Core Space Sponsorship Mechanism' },
+      { url: '/docs/category/tutorials', text: 'Developer Tutorials', preview: 'Tutorials for build Conflux Core Space' },
+      { url: '/docs/core/conflux_rpcs', text: 'Core Space RPC Endpoints', preview: 'RPC endpoint, block explorer url, chainId etc' },
       { url: '/docs/category/build', text: 'Build', preview: 'Learn how to build on Core Space' },
     ]
   },
