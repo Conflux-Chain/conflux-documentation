692e2c7303a87a6ef3655191668b5345e534b0ed
darwintree <wenda.zhang@confluxnetwork.org>
2024-04-12 10:11:01 +0800

    fix: default remove trailing slash

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,6 +15,7 @@ const config = {
   onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/logo.svg',
+  trailingSlash: false,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
