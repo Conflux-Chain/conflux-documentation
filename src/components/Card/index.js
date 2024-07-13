16cb4a6fd8065aaa032c220a75dd65bda249aa8f
0xffffb <0xffffb@gmail.com>
2024-07-13 15:59:58 +0800

    feat: add i18n to js and tsx files
    resolve #375

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,20 +1,30 @@
-import React from 'react';
-import cx from 'clsx';
+import React from "react";
+import cx from "clsx";
+import Translate from "@docusaurus/Translate";
 // import styles from './styles.module.css';
 
 const Card = ({ Image, title, description, className, link }) => {
   return (
-    <div className={cx("flex flex-col items-center rounded-[20px] bg-white dark:bg-[#152748] overflow-hidden drop-shadow-[0px_4px_12px_var(--ifm-color-primary-darker)] w-11/12", className)}>
+    <div
+      className={cx(
+        "flex flex-col items-center rounded-[20px] bg-white dark:bg-[#152748] overflow-hidden drop-shadow-[0px_4px_12px_var(--ifm-color-primary-darker)] w-11/12",
+        className
+      )}
+    >
       {/* <div className="w-full pointer-events-none">
         {Image()}
       </div> */}
       <div className="px-8 pb-4 pt-6">
-        <h3 className='font-medium text-[20px]'>{title}</h3>
-        <p className='text-[#737A88] dark:text-[#A2A8B6] text-[14px] mt-1 font-normal'>{description}</p>
-        <a href={link}>Read the Docs</a>
+        <h3 className="font-medium text-[20px]">{title}</h3>
+        <p className="text-[#737A88] dark:text-[#A2A8B6] text-[14px] mt-1 font-normal">
+          {description}
+        </p>
+        <a href={link}>
+          <Translate id="home.card.readTheDocs">Read the Docs</Translate>
+        </a>
       </div>
     </div>
   );
-}
+};
 
 export default Card;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,49 +1,58 @@
-import React from 'react';
-import cx from 'clsx';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
-import DocCatalog from '@site/src/modules/docCatalog';
-import FeatureCards from '@site/src/modules/featureCards';
-import CFX from '@site/static/img/conflux.svg';
-import styles from './index.module.css';
-
+import React from "react";
+import cx from "clsx";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
+import DocCatalog from "@site/src/modules/docCatalog";
+import FeatureCards from "@site/src/modules/featureCards";
+import CFX from "@site/static/img/conflux.svg";
+import styles from "./index.module.css";
+import Translate, { translate } from "@docusaurus/Translate";
 
 const Home = () => {
   const { siteConfig } = useDocusaurusContext();
 
   return (
-
-    <Layout
-      title={`${siteConfig.title}`}
-      description=""
-    >
-
-      <main className='bg-[#F6F9FF] dark:bg-[#0C1831] flex flex-col items-center justify-center pb-24' >
-        <div className={cx('w-full flex justify-center', styles.gradientBg)}>
-          <div className='max-w-[1920px] w-full px-[76px] flex flex-col lg:flex-row lg:justify-between items-center z-10'>
-            <div className='flex flex-col lg:w-[37.5%] order-2 lg:order-1 font-bold'>
+    <Layout title={`${siteConfig.title}`} description="">
+      <main className="bg-[#F6F9FF] dark:bg-[#0C1831] flex flex-col items-center justify-center pb-24">
+        <div className={cx("w-full flex justify-center", styles.gradientBg)}>
+          <div className="max-w-[1920px] w-full px-[76px] flex flex-col lg:flex-row lg:justify-between items-center z-10">
+            <div className="flex flex-col lg:w-[37.5%] order-2 lg:order-1 font-bold">
               <h1 className="text-[68px]">Conflux</h1>
-              <h1 className="text-[68px]">Developer</h1>
-              <h1 className="text-[68px]">Portal</h1>
-              <p className="hero__subtitle text-[#737A88] text-[24px] dark:text-[#A2A8B6] font-normal mt-6">{siteConfig.tagline}</p>
-              <a href='/docs/overview'>
-                <button className={cx('text-[18px]', styles.startBuildingBtn)}>Start Building</button>
+              <h1 className="text-[68px]">
+                <Translate id="home.developer">Developer</Translate>
+              </h1>
+              <h1 className="text-[68px]">
+                <Translate id="home.portal">Portal</Translate>
+              </h1>
+              <p className="hero__subtitle text-[#737A88] text-[24px] dark:text-[#A2A8B6] font-normal mt-6">
+                <Translate>{siteConfig.tagline}</Translate>
+              </p>
+              <a href="/docs/overview">
+                <button className={cx("text-[18px]", styles.startBuildingBtn)}>
+                  <Translate id="home.startBuilding">Start Building</Translate>
+                </button>
               </a>
             </div>
-            <CFX role='img' className='w-10/12 lg:w-[43.3%] h-auto order-1 lg:order-2' />
+            <CFX
+              role="img"
+              className="w-10/12 lg:w-[43.3%] h-auto order-1 lg:order-2"
+            />
           </div>
         </div>
-        
-        <FeatureCards wrapperClassName='mt-20 lg:mt-0 xl:-translate-y-14 xl:-translate-y-12 z-10' />
-        
-        <div className='px-9 md:px-[78px] w-full max-w-[1920px]'>
-          <div className='text-[32px] font-semibold'>Explore the documentation</div>
+
+        <FeatureCards wrapperClassName="mt-20 lg:mt-0 xl:-translate-y-14 xl:-translate-y-12 z-10" />
+
+        <div className="px-9 md:px-[78px] w-full max-w-[1920px]">
+          <div className="text-[32px] font-semibold">
+            <Translate id="home.ExploreDocumentation">
+              Explore the documentation
+            </Translate>
+          </div>
           <DocCatalog />
         </div>
       </main>
     </Layout>
-
   );
-}
+};
 
 export default Home;
