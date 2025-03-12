755e24754fc99b5abc5ae74d1e152a3ca2da805b
iosh <ioshx@foxmail.com>
2025-03-12 22:08:47 +0800

    chore: fix dark mode color

--- plugins/docusaurus-openrpc/src/components/Content/index.tsx
+++ plugins/docusaurus-openrpc/src/components/Content/index.tsx
@@ -5,31 +5,41 @@ import {
   MethodObjectParams,
   Servers,
 } from "@open-rpc/meta-schema";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Params from "../Params";
 import Examples from "../Examples";
 import "./index.css";
 import { Result } from "../Result";
 import Playground from "../Playground";
-
-import "bootstrap/dist/css/bootstrap.min.css";
+import { useColorMode } from "@docusaurus/theme-common";
+import { StyleProvider } from "@ant-design/cssinjs";
+import { ConfigProvider, theme } from "antd";
 interface Props {
   method: MethodObject;
   servers: { name: string; url: string }[];
 }
 
 const Content = ({ method, servers }: Props) => {
+  const { colorMode } = useColorMode();
+
   return (
-    <div>
-      <p>{method.summary}</p>
-      <Playground
-        method={method}
-        servers={servers.filter((serve) => serve.url.startsWith("http"))}
-      />
-      <Params params={method.params} />
-      <Result result={method.result} />
-      <Examples examples={method.examples} method={method} />
-    </div>
+    <ConfigProvider
+      theme={{
+        algorithm:
+          colorMode === "dark" ? theme.darkAlgorithm : theme.defaultAlgorithm,
+      }}
+    >
+      <StyleProvider>
+        <p>{method.summary}</p>
+        <Playground
+          method={method}
+          servers={servers.filter((serve) => serve.url.startsWith("http"))}
+        />
+        <Params params={method.params} />
+        <Result result={method.result} />
+        <Examples examples={method.examples} method={method} />
+      </StyleProvider>
+    </ConfigProvider>
   );
 };
 
